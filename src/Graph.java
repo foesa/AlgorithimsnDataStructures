1abf8b253498d916ff855756cc674841fb853d68
foesa <leguavoen@gmail.com>
2019-04-03 19:14:55 +0100

    Finished Graph class

--- src/Graph.java
+++ src/Graph.java
@@ -1,2 +1,29 @@
+import java.util.LinkedList;
+
 public class Graph {
+    int vertices;
+    LinkedList<Edge>[] adjacencyList;
+    public static class Edge{
+        int source;
+        int dest;
+        int weight;
+
+        public Edge(int source,int dest,int weight){
+            this.source = source;
+            this.dest = dest;
+            this.weight = weight;
+        }
+    }
+    public Graph(int vertices){
+        this.vertices = vertices;
+        adjacencyList = new LinkedList[vertices];
+        for(int i=0;i<vertices;i++){
+            adjacencyList[i] = new LinkedList<>();
+        }
+    }
+
+    public void addEdge(int source,int dest, int weight){
+        Edge newEdge = new Edge(source,dest,weight);
+        adjacencyList[source].addFirst(newEdge);
+    }
 }
